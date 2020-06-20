d7bb2ea11c3e40f048374b88a041d661d781e277
vijay <vijay@192.168.0.15>
2020-06-20 10:52:14 +0100

    Renaming a class

--- SpringSecurityDemo/src/main/java/com/vijayforvictory/MyUserDetailsService.java
+++ SpringSecurityDemo/src/main/java/com/vijayforvictory/MyUserDetailsService.java
@@ -9,7 +9,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MyuserDetailsService implements UserDetailsService {
+public class MyUserDetailsService implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
